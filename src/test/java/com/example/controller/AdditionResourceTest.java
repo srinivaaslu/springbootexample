ad257a64465b6704fad015e9b65da9e9e2e4d202
Srinivasulu Vankam <srinivasulu.vankam@ing.com>
2019-05-22 16:31:04 +0200

    First Junit added

--- src/test/java/com/example/controller/AdditionResourceTest.java
+++ src/test/java/com/example/controller/AdditionResourceTest.java
@@ -5,12 +5,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = AdditionResource.class, secure = false)
@@ -33,6 +37,10 @@ public class AdditionResourceTest {
 
 
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+       assertEquals(200, response.getStatus());
+
     }
 
 }
