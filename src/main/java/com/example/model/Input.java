cf54856fa7d605708272274546c53351113cba27
Srinivasulu Vankam <srinivasulu.vankam@ing.com>
2019-05-22 16:25:59 +0200

    Junit sample added

--- src/main/java/com/example/model/Input.java
+++ src/main/java/com/example/model/Input.java
@@ -4,8 +4,24 @@ package com.example.model;
  * Created by pf46pnd on 22/05/2019.
  */
 
-public class Input {
-    public int number1;
-    public int number2;
 
+public class Input {
+    public String number1;
+    public String number2;
+
+    public String getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(String number1) {
+        this.number1 = number1;
+    }
+
+    public String getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(String number2) {
+        this.number2 = number2;
+    }
 }
--- src/test/java/com/example/controller/AdditionResourceTest.java
+++ src/test/java/com/example/controller/AdditionResourceTest.java
@@ -1,14 +1,19 @@
 package com.example.controller;
 
+import com.example.model.Input;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(value = AdditionResource.class, secure = false)
 public class AdditionResourceTest {
 
     @Autowired
@@ -16,8 +21,18 @@ public class AdditionResourceTest {
 
 
     @Test
-    public void testAdd(){
+    public void testAdd()throws Exception{
+        Input input = new Input();
+        input.setNumber1("1");
+        input.setNumber2("2");
 
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/sum")
+                .accept(MediaType.APPLICATION_JSON).content(String.valueOf(input))
+                .contentType(MediaType.APPLICATION_JSON);
+
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
     }
 
 }
